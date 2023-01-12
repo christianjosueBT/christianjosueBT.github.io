7ea62879f36be9cfe2f798d4b1314bff7d5575e9
christianjosueBT <christianjosuebt@hotmail.com>
2023-01-12 16:46:20 -0700

    Added Vitest!

--- wordle-clone/src/App.tsx
+++ wordle-clone/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className='mx-auto w-96'>
-      <h1 className='text-4xl text-center'>Wordle</h1>
+      <h1 className='text-4xl text-center'>Wordle!</h1>
     </div>
   )
 }
