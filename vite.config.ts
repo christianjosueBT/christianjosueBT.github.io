58318fb63f75c4fee2a1e07be62959071e8ac81a
christianjosueBT <christianjosuebt@hotmail.com>
2023-02-07 11:41:29 -0700

    Finished! Final touches. Deployed on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/worldle-clone/',
   plugins: [
     react({
       include: ['**/*.tsx', '**/**/*.tsx'],
